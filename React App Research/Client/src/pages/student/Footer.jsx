416df6caffae94732f18a2e2dae5bd676b165d37
APM <>
2022-05-29 21:49:52 +0530

    Add files to the calendar

--- React App Research/Client/src/pages/student/Footer.jsx
+++ React App Research/Client/src/pages/student/Footer.jsx
@@ -8,7 +8,7 @@ import {ImWhatsapp} from "react-icons/im";
 import { ImSkype } from "react-icons/im";
 import { ImLinkedin } from "react-icons/im";
 import { HiPhone} from "react-icons/hi";
-
+import Calen from '../calendars/calen.jsx';
 
 const Footer = () => {
 return (
@@ -58,16 +58,16 @@ return (
           <div class="content">
             <div class="place">
               <span class="fas fa-map-marker-alt"></span>
-              <span class="text"><HiLocationMarker className='card-text text-end text-danger mt-3 mb-3'/>  Mathugama Kaluthara,Colombo</span>
+              <span class="text"><HiLocationMarker className='card-text text-end text-danger mt-3 mb-3'/>  </span>
             </div>
             <div class="phone">
 			
               <span class="fas fa-phone-alt"></span>
-              <span class="text"><HiPhone className='card-text text-end text-danger mt-3 mb-3'/>  +947 88855086</span>
+              <span class="text"><HiPhone className='card-text text-end text-danger mt-3 mb-3'/></span>
             </div>
             <div class="email">
               <span class="fas fa-envelope"></span>
-              <span class="text"><HiMailOpen className='card-text text-end text-danger mt-3 mb-3'/>  info@cdazzdev.com</span>
+              <span class="text"><HiMailOpen className='card-text text-end text-danger mt-3 mb-3'/>  </span>
             </div>
           </div>
         </div>
@@ -76,20 +76,8 @@ return (
         <div class="right box">
           <h2>Contact us</h2>
           <div class="content">
-            {/* <form action="#">
-              <div class="email">
-                <div class="text">Email *</div>
-                <input type="email" required/>
-              </div>
-              <div class="msg">
-                <div class="text">Message *</div>
-                <textarea rows="2" cols="25" required></textarea>
-              </div>
-              <div class="btn">
-                <button type="submit">Send</button>
-              </div>
-            </form> */}
-          </div>
+            <Calen/> 
+         </div>
         </div>
       </div>
       <div class="bottom">
