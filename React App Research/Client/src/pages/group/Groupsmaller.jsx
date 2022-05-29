bd2d526b4b67d2fe30b4f332fcaca652b04aaa32
APM <>
2022-05-29 14:46:38 +0530

    add

--- React App Research/Client/src/pages/group/Groupsmaller.jsx
+++ React App Research/Client/src/pages/group/Groupsmaller.jsx
@@ -1,55 +1,70 @@
 import React from 'react'
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // import {  FaStar } from 'react-icons/fa';
+import React from "react";
+import ReactDOM from "react-dom";
+import { withStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+
+
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import { Card } from '@mui/material';
+import { color } from '@mui/system';
 
 const PackageSmallView = (props) => {
 
     const user = props.user;
     const count = props.count;
 
-    const history = useHistory();
+    const history = useNavigate();
     const handleClick = (path) => {
         history.push(path);
     }
 
     return (
-        // <div className={`col-xs-12 col-sm-6 col-md-${count}`}>
-            
-        //     <div class="card">
-        //         <img src={user.imageURL} class="card-img-top" alt={user.empNumber} />
-        //         <div class="card-body">
-        //             <h5 class="card-title text-center">{user.groupid}</h5>
-        //             <h6 class="card-title">Group ID: {user.department}</h6>
-        //             <p class="card-text">Leader:{user.memberLeader}</p> 
-        //         <div class="card-footer">
-        //                 <div class="row text-center">
-        //                     <div className="">
-        //                         <button onClick={() => handleClick(`/user/${user._id}`)} type="button" class="btn btn-outline-primary">View</button>
-                                
-        //                     </div>    
-        //                 </div>
-                        
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>'
- <div class="container">
-  <div class="card">
-    <div class="box">
-      <div class="content">  
-     <h1>Research Group</h1>
-     <br/> <button type="button" class="btn btn-default btn-circle btn-lg">
-     <h1>{user.groupid}</h1>
-                            </button>
+      <div>
+   <Card
+   sx={{backgroundColor:"black",
+  color:"white",
+  padding:"10px",
+  margin_left:"10px",
+  
+ 
+   }}>
+      <CardContent
+      sx={{backgroundColor:"black",
+      color:"white",
+      padding:"10px",
+
+      }}>
+        <Typography
+          >
+          {user.groupid}
+        </Typography>
+
+        <Typography  
+        >
+          {user.department}
+        </Typography>
+        <br/>
+        <Typography>
+          {user.memberLeader}
+        </Typography>
+        <Typography>
+          {user.memberLeader}
+        </Typography>
+     
+      </CardContent> 
+    </Card>
+<br/>
    
-    <br/>
-      <h3>Department: {user.department}</h3>
-       <h3>Leader : {user.memberLeader}</h3> 
-        <a href="#">Read More</a>
-      </div>
     </div>
-  </div>
-  </div>
+  
+
+
     );
 }
 
